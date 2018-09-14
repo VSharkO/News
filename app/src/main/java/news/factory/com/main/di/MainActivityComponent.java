26cb410ea65385520322eeab22f259a4369d93c1
VSharkO <valentin.saric@hotmail.com>
2018-09-14 09:27:53 +0200

    removed networking module from MainModule and FragmentModule

--- app/src/main/java/news/factory/com/main/di/MainActivityComponent.java
+++ app/src/main/java/news/factory/com/main/di/MainActivityComponent.java
@@ -4,11 +4,10 @@ import dagger.Subcomponent;
 import dagger.android.AndroidInjector;
 import news.factory.com.componentBuilders.ArticleFragmentBuilder;
 import news.factory.com.main.view.MainActivity;
-import news.factory.com.networking.di.NetworkingModule;
 import news.factory.com.scopes.PerActivity;
 
 @PerActivity
-@Subcomponent(modules = {MainActivityModule.class,NetworkingModule.class, ArticleFragmentBuilder.class})
+@Subcomponent(modules = {MainActivityModule.class, ArticleFragmentBuilder.class})
 public interface MainActivityComponent extends AndroidInjector<MainActivity>{
     @Subcomponent.Builder
     abstract class Builder extends AndroidInjector.Builder<MainActivity>{}
