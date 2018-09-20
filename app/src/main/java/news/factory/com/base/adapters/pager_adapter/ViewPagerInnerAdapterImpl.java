415e5865388c2da471f19a7d90f38c4d200b1233
VSharkO <valentin.saric@hotmail.com>
2018-09-20 08:41:58 +0200

    Added page tab strip

--- app/src/main/java/news/factory/com/base/adapters/pager_adapter/ViewPagerInnerAdapterImpl.java
+++ app/src/main/java/news/factory/com/base/adapters/pager_adapter/ViewPagerInnerAdapterImpl.java
@@ -9,6 +9,7 @@ import android.view.ViewGroup;
 import javax.inject.Inject;
 
 import news.factory.com.inner_pager_fragment.view.InnerPagerFragment;
+import news.factory.com.utils.Constants;
 
 public class ViewPagerInnerAdapterImpl extends FragmentStatePagerAdapter implements ViewPagerInnerAdapter{
     private int countNumber;
@@ -37,6 +38,22 @@ public class ViewPagerInnerAdapterImpl extends FragmentStatePagerAdapter impleme
     public void setDataCount(int numOfPages) {
         countNumber = numOfPages;
         notifyDataSetChanged();
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        switch (position){
+            case 0:
+                return Constants.MOST_POPULAR_TYPE.substring(0,1).toUpperCase()
+                        + Constants.MOST_POPULAR_TYPE.substring(1);
+            case 1:
+                return Constants.MOST_READ_TYPE.substring(0,1).toUpperCase()
+                        + Constants.MOST_READ_TYPE.substring(1);
+            case 2:
+                return Constants.MOST_NEW_TYPE.substring(0,1).toUpperCase()
+                        + Constants.MOST_NEW_TYPE.substring(1);
+                default: return "";
+        }
     }
 
 }
